bfd5ca612d71237f1733ebd4f178d4c34ce6499c
ERROR404 <50117177+E-R-R-O-R-404@users.noreply.github.com>
2021-01-31 12:39:18 -0500

    Update settings.py

--- initcrew/initcrew/settings.py
+++ initcrew/initcrew/settings.py
@@ -133,6 +133,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'initcrew/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 ]
 
 #HTTPS SETTINGS
